c92722f89313165beee59505eb806e2e688434d6
Jobelle Ritche Albutra Cueme <55225816+jojosfplay99@users.noreply.github.com>
2024-12-27 06:14:51 +0000

    Update

--- elmos/src/App.tsx
+++ elmos/src/App.tsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
 import '@fontsource/lexend'
+
+//Import Components
+import SearchAppBar from './components/navbar-component'
+
 import './App.css'
 
 function App() {
   return (
     <>      
+      <SearchAppBar></SearchAppBar>
       <h1>Vite + React</h1>
      
     </>
